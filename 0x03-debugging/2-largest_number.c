4a33c61dd08fe0751c5e31e2dbbcd92add8ea4ea
yvettetuyizere <yvettetuyizere@gmail.com>
2023-02-23 10:13:08 -0800

    2-largest_number.c

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -11,11 +11,12 @@
 int largest_number(int a, int b, int c)
 {
 	int largest;
-	if (a > b && a > b)
+
+	if (a >= b && a >= c)
 	{
 		largest = a;
 	}
-	else if (b > a && b > c)
+	else if (b >= a && b >= c)
 	{
 		largest = b;
 	}
@@ -23,5 +24,6 @@ int largest_number(int a, int b, int c)
 	{
 		largest = c;
 	}
-	return (largest)
+
+	return (largest);
 }
