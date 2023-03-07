f6736f683cbbcc63d9bfcd582de5e40dccc22a81
yvettetuyizere <yvettetuyizere@gmail.com>
2023-03-07 08:26:16 -0800

    0-memset.c

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,11 +1,21 @@
 #include "main.h"
+
 /**
- * set_string - sets the value of a pointer to a char
- * @s: pointer to pointer
- * @to: pointer to char
- * Return: void
+ * *_memset - fills memory with a constant byte.
+ * @s: pointer to put the constant
+ * @b: constant
+ * @n: max bytes to use
+ * Return: s
  */
-void set_string(char **s, char *to)
+
+char *_memset(char *s, char b, unsigned int n)
 {
-	*s = to;
+	unsigned int i;
+
+	for (i = 0; n > 0; i++, n--)
+	{
+		s[i] = b;
+	}
+
+	return (s);
 }
